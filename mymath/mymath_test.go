d6c89b997ab510c3b0f1e8661986e1d95c9af135
TW-MONEYSQ\evanfang <evanfang@moneysq.com>
2018-12-20 11:29:06 +0800

    Use run() to group related tests

--- mymath/mymath_test.go
+++ mymath/mymath_test.go
@@ -7,66 +7,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// 1 + 2 = 3
-func TestOnePlusTwoEqualsThree(t *testing.T) {
-	// Arrange
-	num1 := 1
-	num2 := 2
+func TestMyMathModules(t *testing.T) {
 
-	expect := 3 // Expected output of Add(1,2)
-	actual := 0 // Actual output
+	t.Run("1 + 2 should be 3", func(t *testing.T) {
+		expect := 3
+		actual := mymath.Add(1, 2)
+		assert.Equal(t, expect, actual)
+	})
 
-	// Act
-	actual = mymath.Add(num1, num2)
+	t.Run("1 + 1 should be 2", func(t *testing.T) {
+		expect := 2
+		actual := mymath.Add(1, 1)
+		assert.Equal(t, expect, actual)
+	})
 
-	// Assert
-	assert.Equal(t, expect, actual)
-}
+	t.Run("2 x 3 should be 6", func(t *testing.T) {
+		expect := 6
+		actual := mymath.Multiply(2, 3)
+		assert.Equal(t, expect, actual)
+	})
 
-// 1 + 1 = 2
-func TestOnePlusOneEqualsTwo(t *testing.T) {
-	// Arrange
-	num1 := 1
-	num2 := 1
-
-	expect := 2 // Expected output of Add(1,2)
-	actual := 0 // Actual output
-
-	// Act
-	actual = mymath.Add(num1, num2)
-
-	// Assert
-	assert.Equal(t, expect, actual)
-}
-
-// 2 * 3 = 6
-func Test2x3Equals6(t *testing.T) {
-	// Arrange
-	num1 := 2
-	num2 := 3
-
-	expect := 6 // Expected output of Add(1,2)
-	actual := 0 // Actual output
-
-	// Act
-	actual = mymath.Multiply(num1, num2)
-
-	// Assert
-	assert.Equal(t, expect, actual)
-}
-
-// 3 * 4 = 12
-func Test3x4Equals12(t *testing.T) {
-	// Arrange
-	num1 := 3
-	num2 := 4
-
-	expect := 12 // Expected output of Add(1,2)
-	actual := 0  // Actual output
-
-	// Act
-	actual = mymath.Multiply(num1, num2)
-
-	// Assert
-	assert.Equal(t, expect, actual)
+	t.Run("3 x 4 should be 12", func(t *testing.T) {
+		expect := 12
+		actual := mymath.Multiply(3, 4)
+		assert.Equal(t, expect, actual)
+	})
 }
